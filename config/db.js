cd6df754ab0c016262849d41827098f61e6bcfcc
sinansinu07 <mohammedsinanchinnu07@gmail.com>
2024-08-29 05:57:57 +0400

    mongodb url changed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const configureDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/students-grade-system')
+        await mongoose.connect(process.env.MONGODB_REMOTE)
         console.log('Connected to MongoDB')
     } catch(err) {
         console.error('Error Connecting to MongoDB', err)
